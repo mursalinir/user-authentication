06f0ce971119f2f0dab5d2c18a0698a574764367
Mursalin Islam <mursalinir@gmail.com>
2022-10-09 21:03:53 +0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/", to: "users#index"
+  get "/dashboard", to: "home#index"
+  post "/sessions", to: "sessions#create"
+  post "/users", to: "users#create"
   # Defines the root path route ("/")
   # root "articles#index"
 end
